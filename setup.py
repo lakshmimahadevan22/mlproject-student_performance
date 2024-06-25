85af08051b1ef7ee79029387a0a06edadb284ac3
Lakshmi Mahadevan <lakshmimahadevan222@gmail.com>
2024-06-25 20:57:58 +0530

    logging and exception

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
+    return requirements
+    #requirements=[]
+    #with open(file_path) as file_obj:
+     #   requirements=file.obj.readlines()
+      #  requirements=[req.replace("\n","") for req in requirments]
+
+       # if HYPEN_E_DOT in requirements:
+        #    requirements.remove(HYPEN_E_DOT)
+    #return requirements
+    
+# utils.py or wherever you define your helper functions
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Lakshmi',
+author_email='lakshmimahadevan222@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+
+
+
+)
---
+++ src/exception.py
@@ -0,0 +1,34 @@
+import traceback
+
+def error_message_detail(error):
+    tb = traceback.TracebackException.from_exception(error)
+    tb_stack = tb.stack[-1]  # Get the last call in the stack trace
+    file_name = tb_stack.filename
+    line_number = tb_stack.lineno
+    error_message = f"Error occurred in python script name [{file_name}] line number [{line_number}] error message [{str(error)}]"
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message):
+        super().__init__(error_message)
+        self.error_message = error_message
+
+    def __str__(self):
+        return self.error_message
+
+try:
+    # Code that might raise an exception
+    raise ValueError("An example error")
+except Exception as e:
+    error_detail = error_message_detail(e)
+    raise CustomException(error_detail)
+
+
+#if __name__=="__main__":
+
+ #   try:
+  #      a=1/0
+   # except Exception as e:
+    #    logging.info("Divide by Zero")
+     #   raise CustomException(e)
+    
